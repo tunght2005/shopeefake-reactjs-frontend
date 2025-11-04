9a9a5c0b65086fdb81cf54230faf88c4868f3130
Tung DEV <tunght2005@gmail.com>
2025-11-05 01:41:53 +0700

    feat: add Validate by React Hook Form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,58 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
-  return <div>Login</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm() // return register, handleSubmit, formState: {errors}
+  const onSubmit = handleSubmit((data) => {
+    {
+      console.log(data)
+    }
+  })
+  return (
+    <div className='bg-[#ee4d2d]'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-12 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit}>
+              <div className='text-xl'>Đăng Nhập</div>
+              <div className='mt-8'>
+                <input
+                  name='email'
+                  type='email'
+                  className='mt-8 w-full border border-gray-200 rounded px-3 py-2'
+                  placeholder='Email'
+                />
+                <input
+                  name='password'
+                  type='password'
+                  className='mt-2 w-full border border-gray-200 rounded px-3 py-2'
+                  placeholder='Password'
+                  autoComplete='on'
+                />
+                <div className='mt-3'>
+                  <button
+                    type='submit'
+                    className='flex w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
+                  >
+                    Đăng nhập
+                  </button>
+                </div>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+                <Link className='ml-1 text-red-400' to='/register'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
-export default function Register() {
-  return <div>Register</div>
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm() // return register, handleSubmit, formState: {errors}
+  const onSubmit = handleSubmit((data) => {
+    {
+      console.log(data)
+    }
+  })
+  console.log(errors)
+  return (
+    <div className='bg-[#ee4d2d]'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-12 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form action='' className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
+              {' '}
+              {/*noValidate thuộc tính này ngăn cho type email validate*/}
+              <div className='text-xl'>Đăng Nhập</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  className='mt-8 w-full border border-gray-200 rounded px-3 py-2'
+                  placeholder='Email'
+                  {...register('email', { required: true })}
+                />
+                <input
+                  type='password'
+                  className='mt-2 w-full border border-gray-200 rounded px-3 py-2'
+                  placeholder='Password'
+                  autoComplete='on'
+                  {...register('password')}
+                />
+                <input
+                  type='password'
+                  className='mt-2 w-full border border-gray-200 rounded px-3 py-2'
+                  placeholder='Confirm password'
+                  autoComplete='on'
+                  {...register('confirm password')}
+                />
+                <div className='mt-3'>
+                  <button
+                    type='submit'
+                    className='flex w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
+                  >
+                    Đăng ký
+                  </button>
+                </div>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn đã có tài khoản?</span>
+                <Link className='ml-1 text-red-400' to='/login'>
+                  Đăng Nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
