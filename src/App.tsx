1eadee991340688a0bc3569a0ccfcd791be3fe2c
Tung DEV <tunght2005@gmail.com>
2026-05-21 02:26:34 +0700

    feat: handle validation errors by axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import useRouteElement from './useRouteElement'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = useRouteElement()
-  return <>{routeElements}</>
+  return (
+    <>
+      {' '}
+      {routeElements}
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App
