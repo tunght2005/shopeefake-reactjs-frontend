708bbb08fd4f6d17366fde815e5c5db8d4f42f77
Tung DEV <tunght2005@gmail.com>
2026-05-21 02:42:30 +0700

    feat: handle logic api page login

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -2,3 +2,4 @@ import type { AuthResponse } from '../types/auth.type'
 import http from '../utils/http'
 
 export const registerAccount = (body: { email: string; password: string }) => http.post<AuthResponse>('/register', body)
+export const loginAccount = (body: { email: string; password: string }) => http.post<AuthResponse>('/login', body)
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { type LoginSchema, loginSchema } from '../../utils/rules'
 import Input from '../../components/Input'
+import { useMutation } from '@tanstack/react-query'
+import { loginAccount } from '../../apis/auth.api'
+import type { ErrorResponse } from '../../types/utils.type'
+import { isAxiosUnprocessableEntityError } from '../../utils/utils'
 
 export default function Login() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors }
   } = useForm<LoginSchema>({ resolver: zodResolver(loginSchema) }) // return register, handleSubmit, formState: {errors}
+  const loginAccountMutation = useMutation({
+    //Tach confirm_password ra khoi data khi gui len server chi kiem tra no khi dang ki
+    mutationFn: (body: LoginSchema) => loginAccount(body)
+  })
   const onSubmit = handleSubmit((data) => {
-    {
-      console.log(data)
-    }
+    loginAccountMutation.mutate(data, {
+      onSuccess: (data) => {
+        console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntityError<ErrorResponse<LoginSchema>>(error)) {
+          const formError = error.response?.data.data
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof LoginSchema, {
+                message: formError[key as keyof LoginSchema],
+                type: 'Server'
+              })
+            })
+          }
+        }
+      }
+    })
   })
   return (
     <div className='bg-[#ee4d2d]'>
